fa2ef492520c74666f12d5b590f8eabeaa9882d5
Dima G <7088388+dgcom@users.noreply.github.com>
2022-03-12 13:43:55 -0500

    Update up.sh

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# Need to add snap updates
-
 echo =========================================
 echo Updating system
 sudo apt update && sudo apt upgrade -y
 
 echo =========================================
 echo Updating flatpak
-
 sudo flatpak update -y
 
 echo =========================================
 echo Updating snap
-
 sudo snap refresh
 
 echo =========================================
 echo Updating rclone
-
 sudo rclone selfupdate
+
+echo =========================================
+echo autoremove
+sudo apt autoremove -y
+
